2aeecc0ae2c9f79fb672440deaeb146acd7574b3
Jake Thompson <jlcompany23@gmail.com>
2016-08-08 22:43:48 -0600

    working on header

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -16,9 +16,8 @@
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'menu_class' => 'nav navbar-nav collapse navbar-collapse ', 'container' => 'ul' ) ); ?>
 </nav>
 <section id="branding">
-<div id="site-title" class="text-right"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
+<div id="site-title" class="text-right padding-bottom"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
 <div id="site-description" class="padding-bottom text-right"><?php bloginfo( 'description' ); ?></div>
 </section>
 </header>
 <div id="container">
-  
